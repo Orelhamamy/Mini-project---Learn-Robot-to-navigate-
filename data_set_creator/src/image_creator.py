628a0b28d10db963152d13ca67e553f497bd3401
Orel Hamamy <orelhamamy@gmail.com>
2020-10-13 11:15:45 +0300

    Add train model and update

--- data_set_creator/src/image_creator.py
+++ data_set_creator/src/image_creator.py
@@ -9,19 +9,24 @@ from std_msgs.msg import Int16
 import cv2
 import pickle
 
-intervals = 1 # Sec between images
-
+intervals = .75 # Sec between images
+height = 128
+width = 128
 
 class images_buffer():
     def __init__(self, img):
+        img = cv2.resize(img,(width, height))
+        self.img_zero = img
         self.img_one = img
         self.img_two = img
         self.img_three = img
     
     def recive_img(self, new_img):
+        new_img = cv2.resize(new_img,(width, height))
         self.img_three = self.img_two
         self.img_two = self.img_one
-        self.img_one = new_img
+        self.img_one = self.img_zero
+        self.img_zero = new_img
     
     def save_as_jpg(self, path =''):
         time = rospy.get_time()
@@ -33,7 +38,9 @@ class images_buffer():
 
 def get_data(event, img_buffer, bridge):
     img_msg = rospy.wait_for_message('/depth_camera/color/image_raw', Image)
-    img_buffer.recive_img(bridge.imgmsg_to_cv2(img_msg))
+    img = bridge.imgmsg_to_cv2(img_msg)
+    img_buffer.recive_img(img)
+    print(rospy.get_time())
 
 def save_data_set(path, name, data):
     f = open('{}/{}'.format(path, name),'ab')
@@ -42,33 +49,31 @@ def save_data_set(path, name, data):
     print('save {:s}'.format(name))
     
 def main(bridge):
-    st = '{:.0f}'.format(rospy.get_time())
+    # st = rospy.get_time()
     global intervals
     path='/home/lab/Orel_ws/Training_data'
     img_msg = rospy.wait_for_message('/depth_camera/color/image_raw', Image)
     img_buffer = images_buffer(bridge.imgmsg_to_cv2(img_msg))
     get_data_lambada = lambda x: get_data(x, img_buffer, bridge)
     output = []
-    output_with_img = []
-    while not rospy.is_shutdown():
-        img_buffer_update = rospy.Timer(rospy.Duration(intervals), get_data_lambada)
-        key = rospy.wait_for_message('/image_topic/key_press', Int16)
-        
-        time = img_buffer.save_as_jpg(path=path)
-        output_with_img.append([img_buffer.img_one, img_buffer.img_two,
-                         img_buffer.img_three, key.data])
-        output.append([time, key.data])
-        # rospy.spin()
-    save_data_set(path, 'output', output)
-    save_data_set(path, 'session_'+st, output_with_img)
+    # output_with_img = []
+    try:
+        rospy.Timer(rospy.Duration(intervals), get_data_lambada)
+        while not rospy.is_shutdown():
+            key = rospy.wait_for_message('/image_topic/key_press', Int16)
+            time = img_buffer.save_as_jpg(path=path)
+            # output_with_img.append([img_buffer.img_one, img_buffer.img_two,
+            #                         img_buffer.img_three, key.data])
+            output.append([time, key.data])
+            print('img capture at {}'.format(time))      
+    except KeyboardInterrupt:
+        save_data_set(path, 'output', output)
+        # save_data_set(path, 'session_{:.0f}'.format(st), output_with_img)
 
 if __name__=='__main__':
     rospy.init_node('image_creator')
     bridge = CvBridge()
-    try:
-        main(bridge)
-    except rospy.ROSInterruptException:
-        pass
+    main(bridge)
 
 
 
--- data_set_creator/src/teleop_with_keys.py
+++ data_set_creator/src/teleop_with_keys.py
@@ -6,10 +6,13 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Int16
 
 rospy.init_node('teleop_by_arrows')
-pub_vel_cmd = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+pub_vel_cmd = rospy.Publisher('cmd_vel', Twist, queue_size=5)
 key_publisher = rospy.Publisher('/image_topic/key_press', Int16, queue_size=1)
-linear_speed = 0.75
+linear_speed = 0.5
 angular_speed = 0.4
+duration = 1
+
+
 def stop():
     global pub_vel_cmd
     msg = Twist()
@@ -19,13 +22,14 @@ def stop():
 
     
 def move(linear, angular):
-    global pub_vel_cmd
+    # print(rospy.get_time())
+    global pub_vel_cmd, duration
     msg = Twist()
     msg.linear.x = linear 
     msg.angular.z = angular
     pub_vel_cmd.publish(msg)
-    rospy.sleep(2)
-    stop()
+    # rospy.sleep(duration)
+    # stop()
     
         
 def listener():
@@ -35,24 +39,38 @@ def listener():
         while not key:
             True
         if key==keys.UP:
-            move(linear_speed,0)
             key_publisher.publish(1)
+            print(rospy.get_time())
+            move(linear_speed,0)
         elif key==keys.DOWN:
-            move(-linear_speed,0)
             key_publisher.publish(2)
+            print(rospy.get_time())
+            move(-linear_speed,0)
         elif key==keys.RIGHT:
-            move(0,-angular_speed)
             key_publisher.publish(3)
+            print(rospy.get_time())
+            move(0,-angular_speed)
         elif key==keys.LEFT:
-            move(0,angular_speed)
             key_publisher.publish(4)
+            print(rospy.get_time())
+            move(0,angular_speed)
+        elif key=='q':
+            linear_speed = linear_speed*1.1
+        elif key=='a':
+            linear_speed = linear_speed*0.9
+        elif key=='w':
+            angular_speed = angular_speed*1.1
+        elif key=='s':
+            angular_speed = angular_speed*0.9
         key = getkey()
         
 if __name__ == '__main__':
     try:
         listener()
-    except rospy.ROSInterruptException:
-        pass
+    except rospy.ROSInterruptException or KeyboardInterrupt:
+        stop()
     
-    
-
+'''
+hape = (None, image.shape[0], image.shape[1], 3)
+shape = (None, 128,128, 3) 
+'''
